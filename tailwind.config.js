819df0b54ed40ba31bf2343e989f5a5e655e5b91
DanielRodado <d4nielrodado@gmail.com>
2024-07-18 16:31:25 -0500

    Configuraciones

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,js}", "./index.html"],
+    content: ["./public/index.html"],
     darkMode: "class",
     theme: {
         colors: {
